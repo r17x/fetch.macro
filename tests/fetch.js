dcb84daa1bd459fbbb70d16f3efe48979b8a9f9b
RiN <ri7nz@evilfactory.id>
2022-09-08 11:02:17 +0800

    feat: allow parse url arguments to be fetcher parameters (#3)
    * feat: parse url arguments to be function parameters
    
            example:
    
            const fetchUser = f\`/api/v1/user/:id\`
    
            to be
    
            const fetchUser = ({ id, ...opts })
    
    * test: add nested test case
    
    * docs: add nested example
    
    * docs: update description in package.json

--- tests/fetch.js
+++ tests/fetch.js
@@ -21,4 +21,35 @@ createTests("fetch", [
       const fetchByUrl = (opts) => fetch("/api/v1/ping", opts);
     `,
   },
+  {
+    title: "fetch with url args",
+    code: `
+      import f from '../src/fetch.macro'
+      const fetchUserByID = f\`/api/v1/user/:id\`;
+    `,
+    output: `
+      const fetchUserByID = ({ id, ...opts }) => fetch(\`/api/v1/user/\${id}\`, opts);
+    `,
+  },
+  {
+    title: "fetch with url args 2",
+    code: `
+      import f from '../src/fetch.macro'
+      const fetchProject = f\`/api/v1/user/:id/project/:projectId\`;
+    `,
+    output: `
+      const fetchProject = ({ id, projectId, ...opts }) => fetch(\`/api/v1/user/\${id}/project/\${projectId}\`, opts);
+    `,
+  },
+  {
+    title: "fetch with url args nested",
+    code: `
+      import f from '../src/fetch.macro'
+      const fetchProject = f\`/api/v1/user/:id/project/:projectId/:others\`;
+    `,
+    output: `
+      const fetchProject = ({ id, projectId, others, ...opts }) =>
+        fetch(\`/api/v1/user/\${id}/project/\${projectId}/\${others}\`, opts);
+    `,
+  },
 ]);
