1a82c5196fcb9d3164486138c73d15d20bc7963d
r17x <ri7nz@evilfactory.id>
2022-10-03 01:13:19 +0800

    test: create test case for fetchText

--- src/fetch.macro.js
+++ src/fetch.macro.js
@@ -9,47 +9,55 @@ const getValue = (path) =>
 
 const isValueHaveArgs = (val) => /:\w+/g.test(val);
 
-module.exports = createMacro(({ babel: { types: t, template }, references: { default: paths } }) => {
-  paths.forEach(({ parentPath }) => {
-    const value = getValue(parentPath);
+module.exports = createMacro(
+  ({
+    babel: { types: t, template },
+    references: {
+      default: paths,
+      // @TODO fetchText
+    },
+  }) => {
+    paths.forEach(({ parentPath }) => {
+      const value = getValue(parentPath);
 
-    if (value) {
-      if (isValueHaveArgs(value)) {
-        const buildFetch = template(`(PARAM) => fetch(URI, opts)`);
-        parentPath.replaceWithMultiple(
-          buildFetch({
-            PARAM: t.objectPattern(
-              value
-                .split("/")
-                .filter((v) => v.startsWith(":"))
-                .map((p) => {
-                  const id = t.identifier(p.replace(":", ""));
-                  return t.objectProperty(id, id, false, true);
-                })
-                .concat([t.restElement(t.identifier("opts"))]),
-            ),
-            URI: t.templateLiteral(
-              value
-                .replace(/:\w+/g, "::::")
-                .split("::::")
-                .map((v, i, a) => t.templateElement({ raw: v, cooked: v }, i + 1 === a.length)),
-              value
-                .split("/")
-                .filter((v) => v.startsWith(":"))
-                .map((v) => t.identifier(v.replace(":", ""))),
-            ),
-          }),
-        );
+      if (value) {
+        if (isValueHaveArgs(value)) {
+          const buildFetch = template(`(PARAM) => fetch(URI, opts)`);
+          parentPath.replaceWithMultiple(
+            buildFetch({
+              PARAM: t.objectPattern(
+                value
+                  .split("/")
+                  .filter((v) => v.startsWith(":"))
+                  .map((p) => {
+                    const id = t.identifier(p.replace(":", ""));
+                    return t.objectProperty(id, id, false, true);
+                  })
+                  .concat([t.restElement(t.identifier("opts"))]),
+              ),
+              URI: t.templateLiteral(
+                value
+                  .replace(/:\w+/g, "::::")
+                  .split("::::")
+                  .map((v, i, a) => t.templateElement({ raw: v, cooked: v }, i + 1 === a.length)),
+                value
+                  .split("/")
+                  .filter((v) => v.startsWith(":"))
+                  .map((v) => t.identifier(v.replace(":", ""))),
+              ),
+            }),
+          );
+        } else {
+          const buildFetch = template(`(opts) => fetch(URI, opts)`);
+          parentPath.replaceWithMultiple(
+            buildFetch({
+              URI: t.stringLiteral(value),
+            }),
+          );
+        }
       } else {
-        const buildFetch = template(`(opts) => fetch(URI, opts)`);
-        parentPath.replaceWithMultiple(
-          buildFetch({
-            URI: t.stringLiteral(value),
-          }),
-        );
+        parentPath.parentPath.remove();
       }
-    } else {
-      parentPath.parentPath.remove();
-    }
-  });
-});
+    });
+  },
+);
